bfafaa7ada748657495d26d12a0c0da2f6288a71
codexnoise <diegovo@outlook.com>
2026-05-11 09:57:19 -0500

    feat(firebase): add Firebase Analytics dependency and observer

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:truequeapp/features/home/domain/usecases/update_item_usecase.dart';
 import '../../features/auth/data/datasources/auth_remote_data_source.dart';
@@ -66,6 +67,7 @@ Future<void> init() async {
   // External (Third-party plugins)
   sl.registerLazySingleton(() => FirebaseAuth.instance);
   sl.registerLazySingleton(() => FirebaseFirestore.instance);
+  sl.registerLazySingleton(() => FirebaseAnalytics.instance);
 }
 
 void setupHomeDependencies() {
